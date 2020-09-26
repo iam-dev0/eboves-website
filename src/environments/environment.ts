e0d9d33db966dfb66cd9bc7ba9d5f98539778438
saiban ali <saiban.ali0405@gmail.com>
2020-09-26 16:30:52 +0500

    Revert "Revert "card style fixes""
    This reverts commit 3e6afbbb1248977690209d837aa86a0d74f41fbe.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,13 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4040/api/v1/',
+  apiUrl: 'https://api.eboves.com/api/v1/',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
+/** For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
+
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
